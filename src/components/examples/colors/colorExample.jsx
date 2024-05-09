90db793788a501dc0e3ff7ffa8dabaadf5ace88f
Leandro Morrone <lmorrone@artech-consulting.com>
2024-05-09 09:41:13 -0300

    Descripción del uso de colores

--- src/components/examples/colors/colorExample.jsx
+++ src/components/examples/colors/colorExample.jsx
@@ -1,6 +1,8 @@
 export default ({ hex, number, className }) => (
   <div className="flex flex-1 flex-col gap-1">
-    <div className={`flex h-16 flex-col justify-between rounded-md px-2 py-1 ${className}`}>
+    <div
+      className={`flex h-16 flex-col justify-between rounded-md px-2 py-1 ${className} transition-all hover:scale-110`}
+    >
       <div className="font-semibold">{number}</div>
       <div className="text-sm opacity-80">{hex}</div>
     </div>
